7aaa1aef9b2bb4e12b18f6705ddc4dc8e54238f0
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-02-23 17:05:43 +0900

    [Silver III] Title: N과 M (3), Time: 480 ms, Memory: 68140 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] ar;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        ar = new int[M];
+        visited = new boolean[N];
+
+        dfs(1, 0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int at, int depth) {
+        if (depth == M) {
+            for (int i : ar) {
+                sb.append(i).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+
+            ar[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
+
