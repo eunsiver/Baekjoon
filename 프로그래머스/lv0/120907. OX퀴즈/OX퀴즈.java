e057dabee524caa6d4ed16eee833849674b2a0bc
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-02-28 18:15:17 +0900

    [level 0] Title: OX퀴즈, Time: 0.15 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -1,13 +1,13 @@
 class Solution {
     public String[] solution(String[] quiz) {
-      String[] answer = new String[quiz.length];
+     
         int k=0;
         for(int i=0;i<quiz.length;i++){
             String []s=quiz[i].split(" ");
             int result=Integer.parseInt(s[0])+Integer.parseInt(s[2])*(s[1].equals("+")?1:-1);
-            answer[k++]=(result==Integer.parseInt(s[4])?"O":"X");
+            quiz[i]=(result==Integer.parseInt(s[4])?"O":"X");
            
         }
-        return answer;
+        return quiz;
     }
 }
