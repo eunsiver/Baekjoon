ee5621a516d556f09a7cee972bb36c4a0e2e7f69
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-02-28 18:14:29 +0900

    [level 0] Title: OX퀴즈, Time: 0.08 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -3,32 +3,10 @@ class Solution {
       String[] answer = new String[quiz.length];
         int k=0;
         for(int i=0;i<quiz.length;i++){
-
-         
             String []s=quiz[i].split(" ");
-            int a=Integer.parseInt(s[0]);
-            int b=Integer.parseInt(s[2]);
-            int result=Integer.parseInt(s[4]);
-            if(s[1].equals("+")){
-                if(a+b==result)answer[k++]="O";
-                else answer[k++]="X";
-            }
-            else if(s[1].equals("-")){
-                if(a-b==result)answer[k++]="O";
-                else answer[k++]="X";
-            }
-            else if(s[1].equals("*")){
-                if(a*b==result)answer[k++]="O";
-                else answer[k++]="X";
-            }
-            else if(s[1].equals("/")){
-                if(a/b==result)answer[k++]="O";
-                else answer[k++]="X";
-            }
-            else if(s[1].equals("%")){
-                if(a%b==result)answer[k++]="O";
-                else answer[k++]="X";
-            }
+            int result=Integer.parseInt(s[0])+Integer.parseInt(s[2])*(s[1].equals("+")?1:-1);
+            answer[k++]=(result==Integer.parseInt(s[4])?"O":"X");
+           
         }
         return answer;
     }
