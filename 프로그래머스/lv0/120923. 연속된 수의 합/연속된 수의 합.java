f4127194e8335d7e307d8dddceef62cb721e76e9
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-03-03 21:22:00 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] solution(int n, int t) {
         int a=(2*t-n*n+n)/(2*n);
-        System.out.println(a);
         int[] answer=new int[n];
-        for(int i=0;i<n;i++){
-            answer[i]=a+i;
-        }
+        for(int i=0;i<n;i++) answer[i]=a+i;
         return answer;
     }
 }
