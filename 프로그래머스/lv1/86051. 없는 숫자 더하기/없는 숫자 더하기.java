151cea5e3e60b43f6173b60f954089312b2133c9
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-03-07 04:40:21 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer =0;
+         for (int number : numbers) {
+            answer += number;
+        }
+        return 45-answer;
+    }
+}
