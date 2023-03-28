7757b3424dcfae76dfd4bb58608f158364249e7e
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-03-28 18:47:32 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String s) {
+        
+        int len=s.length();
+        
+        if((len!=4&&len!=6))return false;
+        
+        for (int i=0;i<s.length();i++) {
+            if (s.charAt(i) < '0' || s.charAt(i) > '9') return false;
+        }        
+        
+        return true;
+    }
+}
