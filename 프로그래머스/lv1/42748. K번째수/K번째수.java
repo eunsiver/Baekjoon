05064dd204559a54a94e3470c3f083a16dda1ff1
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-04-07 00:36:26 +0900

    [level 1] Title: K번째수, Time: 0.43 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,25 +1,23 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        List<Integer> li=new ArrayList<>();
-         List<Integer> m=new ArrayList<>();
-        int a,b,c;
+        // 특정 구간 잘라내기
+        // 정렬하기
+        // 정렬된 배열에서 k번째 수 구하기
+        int [] answer=new int[commands.length];
         
-        for(int i=0;i<commands.length;i++){
-            li=new ArrayList<>();
-            a = commands[i][0];
-            b = commands[i][1];
-            c = commands[i][2];
+        for(int i=0;i<answer.length;i++){
+            int []command=commands[i];
             
-            for(int j=a-1;j<=b-1;j++){
-                li.add(array[j]);
-            }
-            Collections.sort(li);
-            m.add(li.get(c-1));
+            int from =command[0]-1;
+            int to= command[1];
+            int k=command[2]-1;
             
-            System.out.println();
+            int [] copy=Arrays.copyOfRange(array,from,to);
+            Arrays.sort(copy);
+            answer[i]=copy[k];
         }
-        int[] answer = m.stream().mapToInt(i->i).toArray();
+        
         return answer;
     }
 }
