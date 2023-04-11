55f6c0c7e6bdbdfbc2dcc335ab24a324aae7b695
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-04-11 17:59:59 +0900

    [level 2] Title: 주식가격, Time: 12.99 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,20 +1,6 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
-        
-        int[] answer =new int[prices.length];
-        Arrays.fill(answer,0);
-        
-        for(int i=0;i<prices.length;i++){
-            for(int j=i+1;j<prices.length;j++){
-                
-                answer[i]++;
-                //System.out.printf("%d %d\n",answer[i],answer[j]);
-                if(prices[j]<prices[i])break;
-                
-            }
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
