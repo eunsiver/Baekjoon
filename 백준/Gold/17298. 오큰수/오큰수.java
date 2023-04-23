d3a35b1123e986097167c9ca8f2c359d64d0bec4
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-04-24 01:22:57 +0900

    [Gold IV] Title: 오큰수, Time: 1096 ms, Memory: 175612 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,49 +1,36 @@
-import java.util.Scanner;
-import java.util.Stack;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
-		
-		Scanner in = new Scanner(System.in);
-		Stack<Integer> stack = new Stack<Integer>();
-		
-		int N = in.nextInt();
-		int[] seq = new int[N];
-		
-		for(int i = 0; i < N; i++) {
-			seq[i] = in.nextInt();
-		}
- 
- 
-		for(int i = 0; i < N; i++) {
-			
-			/*
-			 * 스택이 비어있지 않으면서 
-			 * 현재 원소가 스택의 맨 위 원소가 가리키는 원소보다 큰 경우
-			 * 해당 조건을 만족할 때 까지 stack의 원소를 pop하면서
-			 * 해당 인덱스의 값을 현재 원소로 바꿔준다. 
-			 */
-			while(!stack.isEmpty() && seq[stack.peek()] < seq[i]) {
-				seq[stack.pop()] = seq[i];
-			}
-			
-			stack.push(i);
-		}
-		
-		/*
-		 * 스택의 모든 원소를 pop하면서 해당 인덱스의 value를
-		 * -1로 초기화한다.
-		 */
-		while(!stack.isEmpty()) {
-			seq[stack.pop()] = -1;
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < N; i++) {
-			sb.append(seq[i]).append(' ');
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack= new Stack<>();
+
+        int n= Integer.parseInt(br.readLine());
+        long ar[]=new long[n];
+        StringTokenizer st= new StringTokenizer(br.readLine());
+
+        for (int i = 0; i <n ; i++) {
+            ar[i]= Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i <n; i++) {
+
+            while(!stack.isEmpty()&&ar[stack.peek()]<ar[i]){
+                ar[stack.pop()]=ar[i];
+            }
+            stack.push(i);
+        }
+
+        for(int i: stack){
+            ar[i]=-1;
+        }
+        StringBuilder sb= new StringBuilder();
+        for (long k:ar) {
+            sb.append(k+" ");
+        }
+        System.out.println(sb);
+    }
 }
- 
