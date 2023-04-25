4708c84bdd7a273bc2d34862edc1fcbea8941e2b
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-04-25 16:39:04 +0900

    [Bronze II] Title: 방 배정, Time: 120 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int [][]team=new int[7][2];
+
+        String []s=br.readLine().split(" ");
+
+        int n=Integer.parseInt(s[0]);
+        int capacity=Integer.parseInt(s[1]);
+        StringTokenizer st;
+
+        for(int i=0;i<n;i++){
+            st=new StringTokenizer(br.readLine());
+            int a=Integer.parseInt(st.nextToken());
+            int b=Integer.parseInt(st.nextToken());
+
+            team[b][a]++;
+        }
+        int sum=0;
+        for(int i=1;i<=6;i++){
+            sum+=team[i][0]/capacity+team[i][0]%2;
+            sum+=team[i][1]/capacity+team[i][1]%2;
+        }
+        //System.out.println(Arrays.deepToString(team));
+        System.out.println(sum);
+    }
+}
