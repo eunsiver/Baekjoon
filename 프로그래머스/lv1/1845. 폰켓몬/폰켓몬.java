9bf8ac1f53b072d23dfc19e5313f1ef85c5fbe40
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-08-26 21:40:32 +0900

    [level 1] Title: 폰켓몬, Time: 9.22 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,23 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         
-        Map<Integer, Integer> map=new HashMap<>();
+        HashSet<Integer> hs = new HashSet<>();
         
-        for(int n:nums){
-            map.putIfAbsent(n,0);
-            map.put(n,map.get(n)+1);
-        }
-        System.out.println(map);
+        for(int i: nums) hs.add(i);
         
-        int getNum=nums.length/2;
-        
-        if(map.size()>=getNum) return getNum;
-        else return map.size();
-       
+        int answer = Math.min(hs.size(), nums.length/2);
+        return answer;
     }
 }
 
-//해시맵에 저장
-//배열의 길이 / 2 마리 포켓몬 선택 중 가장 많은 포켓몬 선택
-// hashmap 키의 개수 >= 배열의 길이 / 2 -> 배열의 길이 / 2
-// < -> 키의 개수로 반환
+// N/2 마리
+// HashMap으로 
