de7b6431459650601cc682650887ab70989be4e7
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-08-28 15:23:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.20 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,50 +2,57 @@ import java.util.*;
 
 class Solution {
     
-    int [][]maps;
-    static int isVisited[][];
+    static int N,M;
+    static int [][] map;
+    
+    static class Node{
+        int x,y;
+        
+        Node(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+    
+    static int dx[]={0,1,0,-1};
+    static int dy[]={1,0,-1,0};
     
     public int solution(int[][] maps) {
-        this.maps=maps;
-        isVisited=new int[maps.length][maps[0].length];
         
-        bfs();
-        int result=isVisited[maps.length-1][maps[0].length-1];
-        return result==0?-1:result;
+        N=maps.length;
+        M=maps[0].length;
+        map= maps;
         
-        
+        bfs(0,0);
+        int k=(map[N-1][M-1]);
+       // System.out.println(Arrays.deepToString(map));
+        if(k==1)k=-1;
+        int answer = k;
+        return answer;
     }
-    public void bfs(){
+    
+    public void bfs(int x, int y){
         
-        Queue<int[]>q=new LinkedList<>();
-        int x=0,y=0;
+        Queue<Node> que = new LinkedList<>();
+        que.offer(new Node(x,y));
         
-        q.add(new int[]{y,x});
-        isVisited[y][x]=1;
-        
-        int []dx={0,1,0,-1};
-        int []dy={1,0,-1,0};
-        
-        while(!q.isEmpty()){
-            
-            int[] now=q.remove();
-            x=now[1];
-            y=now[0];
+        while(!que.isEmpty()){
+       
+            Node now = que.poll();
             
             for(int i=0;i<4;i++){
+                int newX = now.x + dx[i];
+                int newY = now.y + dy[i];
                 
-                int X=x+dx[i];
-                int Y=y+dy[i];
-                
-                if(X<0||X>maps[0].length-1||Y<0||Y>maps.length-1){
-                    continue;
-                }
-                
-                if(maps[Y][X]==1&&isVisited[Y][X]==0){
-                    isVisited[Y][X]=isVisited[y][x]+1;
-                    q.add(new int[]{Y,X});
+                if(newX<0||newX>=N||newY<0|| newY>=M)continue;
+                //1인 경우에만 가기
+                if(map[newX][newY]==1){
+                    //System.out.println(newX+"," + newY);
+                    que.offer(new Node(newX, newY));
+                    map[newX][newY]=map[now.x][now.y]+1;
                 }
             }
         }
+        
     }
 }
