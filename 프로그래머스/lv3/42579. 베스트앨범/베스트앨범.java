2584016173d7b0f052f3d15bbfdf5fcc104f942d
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-09-06 00:25:32 +0900

    [level 3] Title: 베스트앨범, Time: 4.21 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -4,7 +4,6 @@ class Solution {
         
       
         List<Integer> result= new ArrayList<>();
-        
         Map<String,Integer> count_map=new HashMap<>();
         Map<Integer,Integer> map=new HashMap<>();
         
@@ -12,37 +11,37 @@ class Solution {
             count_map.put(genres[i],count_map.getOrDefault(genres[i],0)+plays[i]);
         }
         
+        //pop 3100, classic 1450 value의 오름차순으로 sort
         List<Map.Entry<String,Integer>> li= new ArrayList<>(count_map.entrySet());
         li.sort((a,b)->b.getValue()-a.getValue());
         
         System.out.println(li);
-        
+        //모든 장르를 돌면서
         for(Map.Entry<String,Integer> s: li){
             
+            //현재 장르와 같은 것을 map에 인덱스와 plays의 값을 함께 넣음
             map=new HashMap<>();
             for(int i=0;i<genres.length;i++){
                 if(s.getKey().equals(genres[i])){
                     map.put(i,plays[i]);
                 }
             }
+            //현재 장르의 재생 횟수를 sort, 많은 재생 노래 수록 후, 같은 노래는 index부터
             List<Map.Entry<Integer,Integer>> mapLi= new ArrayList<>(map.entrySet());
             mapLi.sort((a,b)->{
                 if(a.getValue()==b.getValue())return a.getKey()-b.getKey();
                 else return b.getValue()-a.getValue();
                  
             });
-            System.out.println(mapLi);
-            
+
+            //답에 넣기
             for(int i=0;i<2;i++){
                 if(!mapLi.isEmpty()){
                    result.add(mapLi.get(0).getKey());
                    mapLi.remove(0);
-             }
+                 }
             }
-          
         }
-        
-        
         return result.stream().mapToInt(i->i).toArray();
     }
 }
