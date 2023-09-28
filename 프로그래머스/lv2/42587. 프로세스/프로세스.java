4e56b3049e956679ef941423ddc9de49d2e7a766
eunsiver <76419984+eunsiver@users.noreply.github.com>
2023-09-28 23:30:33 +0900

    [level 2] Title: 프로세스, Time: 0.94 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.java
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.java
@@ -1,33 +1,26 @@
 import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
+        
         int []pr=Arrays.copyOf(priorities, priorities.length);
-        //내림차순으로 sort하고
         Arrays.sort(priorities);
         List<Integer> result=new ArrayList<>();
-        
-        //큐에 배열을 index값을 넣음
         Deque<Integer> que = new LinkedList<>();
         int n=0;
-        for(int i: priorities){
-            que.offer(n++);
-        }
-        System.out.println(que);
+        
+        for(int i: priorities) que.offer(n++);
+
         for(int i=priorities.length-1;i>=0;i--){
             
-            System.out.println("now"+i);
-            
             while(priorities[i]>pr[que.getFirst()]){
-                System.out.println("priorities[i] : "+priorities[i]+" priorities[que.getFirst()] : " +priorities[que.getFirst()]+" que: " +que);
+               
                 int k= que.removeFirst();
                 que.addLast(k);
             }
             result.add(que.removeFirst());
-            System.out.println(result);
+           
         }
         int rr= result.indexOf(location);
-        
-        System.out.println(rr+1);
         int answer = rr+1;
         return answer;
     }
